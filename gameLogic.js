1ce8b9a822dc0bfcb455f115a982984695302048
Partrickoder <redockirtap@gmail.com>
2022-10-22 12:58:51 +0300

    Add gameFlow module, which is to control  all logic.

--- gameLogic.js
+++ gameLogic.js
@@ -1,19 +1,28 @@
-const Players = (name) => {
-    const player = name || 'John Doe';
-    const sayHello = function () {
-        console.log("hi")
-    }
-    return {player, sayHello};
-}
+const gameFlow = (() => {
+    const Players = (name) => {
+        const player = name || 'John Doe';
+        const marker = document.querySelector(`${cell-$}`);
 
-const gameBoard = (() => {
-    const _board = [x, x, o, o, x, o, x, o, o];
 
-    const addMarker = (marker) => {
-        console.log(marker)
+        const sayHello = function () {
+            console.log("hi")
+        }
+        return {player, marker, sayHello};
     }
+
+    const gameBoard = (() => {
+        const _board = [x, x, o, o, x, o, x, o, o];
+
+        const addMarker = (marker) => {
+            console.log(marker)
+        }
+    })();
+
+    const displayController = (() => {
+        console.log('hi');
+    })();
 })();
-
+    
 const player1 = Players('xxx');
 player1.sayHello();
 console.log(player1.player);
