931c0579f874c9901e6cf5cf07a1a17af82d3a7d
Partrickoder <redockirtap@gmail.com>
2022-10-22 19:37:17 +0300

    Create new branch for refactoring code and its scope(gameFlow module to be parallel with the rest, not its parent).

--- gameLogic.js
+++ gameLogic.js
@@ -1,23 +1,24 @@
 const gameFlow = (() => {
-    const button = document.querySelector('button[class="me"]');
-    console.log(button)
     const Players = (name) => {
         const player = name || 'John Doe';
-        const marker = 'hi' || document.querySelector(`.cells.board-cell-${$}`);
+        // const marker = 'hi' || document.querySelector(`.cells.board-cell-${$}`);
+        const gameStat = []; // to add wins and loses
 
 
         const sayHello = function () {
             console.log("hi")
         }
-        return {player, marker, sayHello};
+        return {player, gameStat, sayHello};
     }
 
     const gameBoard = (() => {
-        const _board = ['x', 'x', 'o', 'o', 'x', 'o', 'x', 'o', 'o'];
+        const _board = ['x', 'x', 'o', 'o', null, 'o', 'x', 'o', 'o'];
 
         const addMarker = (marker) => {
             console.log(marker)
         }
+
+        return {_board};
     })();
 
     const displayController = (() => {
@@ -25,26 +26,35 @@ const gameFlow = (() => {
     })();
 
 
-    const _choosePlayer = function () { // choose player vs player or player vs AI
+    const _choosePlayer = function (e) { // choose player vs player or player vs AI
+        const chosenPlayer = e.target || 'Johnny'; // select player buttons
         console.log('choosing game mode...')
-        // if (player1 && player2) return; // OR if button play game is clicked
-        const chosenPlayer = 'Johnny' || document.querySelector('button'); // select player buttons
-        player1 = Players(chosenPlayer);
+        if (chosenPlayer.textContent === 'play') return; // OR if button play game is clicked
+        player1 = Players(chosenPlayer.textContent);
         // same logic for second player
         return {player1} // return players to parent scope
     };
 
     const _isOver = () => { // checks if game is finished after each step
-        console.log('checking if game is over')
+        if (!gameBoard._board.includes(null)) return;
+        console.log(gameBoard._board);
     }
 
-    const _addMarker = () => { // adds marker to the board, if it is available
-        console.log('adding a marker')
+    const _addMarker = (e) => { // adds marker to the board, if it is available
+        if (e.target === e.currentTarget) return;
+        const chosenCell = e.target;
+        console.log(chosenCell);
     }
 
+    const _gameFlowLogic = () => {
+        const button = document.querySelector('.buttonz');
+        const cells = document.querySelector('.board');
 
-    button.addEventListener('click', _choosePlayer);
-    // player1.sayHello();
+        button.addEventListener('click', _choosePlayer);
+        cells.addEventListener('click', _addMarker);
+        // _addMarker();
+    }
 
+    _gameFlowLogic();
 })();
     
