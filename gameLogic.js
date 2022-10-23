119942a877698f484ba04ba720c1d52cc44e6e91
Partrickoder <redockirtap@gmail.com>
2022-10-23 14:19:45 +0300

    Start adding basic styles and game logic, to get player change after each turn.

--- gameLogic.js
+++ gameLogic.js
@@ -1,26 +1,12 @@
 const Players = (name) => {
     const player = name || 'John Doe';
-    // const marker = 'hi' || document.querySelector(`.cells.board-cell-${$}`);
     const gameStat = []; // to add wins and loses
     return {player, gameStat};
 }
 
 const gameBoard = (() => {
     const _board = ['x', 'x', 'o', 'o', null, 'o', 'x', 'o', 'o'];
-
-    const _addMarker = (e) => { // adds marker to the board, if it is available
-        if (e.target === e.currentTarget) return;
-        const chosenCell = e.target;
-        console.log(chosenCell);
-    }
-
-
-    const _isOver = () => { // checks if game is finished after each step
-        if (!_board.includes(null)) return;
-        console.log(gameBoard._board);
-    }
-
-    return {_board, _addMarker, _isOver};
+    return {_board};
 })();
 
 const displayController = (() => {
@@ -32,20 +18,25 @@ const _gameFlow = (() => {
     const _cells = document.querySelector('.board');
 
     const _choosePlayer = function (e) { // choose player vs player or player vs AI
-        const chosenPlayer = e.target || 'Johnny'; // select player buttons
+        const chosenPlayer = e.target || null; // select player buttons
         console.log('choosing game mode...')
-        if (chosenPlayer.textContent === 'play') return; // OR if button play game is clicked
-        const player1 = Players(chosenPlayer.textContent);
-        // same logic for second player
-        return {player1} // return players to parent scope
+        if (chosenPlayer === null || chosenPlayer.className === 'play') return; // OR if button play game is clicked
+        const player1 = Players('user');
+        const player2 = Players(chosenPlayer.textContent);
+        console.log(player1, player2);
     };
 
-    
+    const _addMarker = (e) => { // adds marker to the board, if it is available
+        if (e.target === e.currentTarget) return;
+        const chosenCell = e.target;
+        console.log(chosenCell);
+    }
+
+    const _isOver = () => { // checks if game is finished after each step
+        if (!_board.includes(null)) return;
+        console.log(gameBoard._board);
+    }
 
     _button.addEventListener('click', _choosePlayer);
-    _cells.addEventListener('click', gameBoard._addMarker);
-    // _addMarker();
-
-    return {player1};
+    _cells.addEventListener('click', _addMarker);
 })();
-
