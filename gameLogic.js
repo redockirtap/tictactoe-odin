fb7426440080168f2ef532ca96896d348f991afe
Partrickoder <redockirtap@gmail.com>
2022-10-23 22:56:42 +0300

    Issue: refactor modules, to have more private, rather than public variables and methods.

--- gameLogic.js
+++ gameLogic.js
@@ -1,23 +1,33 @@
 const Players = (name) => {
     const player = name || 'John Doe';
+    const marker = null;
     const gameStat = []; // to add wins and loses
-    return {player, gameStat};
+    return {player, gameStat, marker};
 }
 
 const gameBoard = (() => {
     const _board = [null, null, null, null, null, null, null, null, null];
-    return {_board};
+
+    const getBoard = () => [..._board];
+    console.log(typeof [..._board], [..._board]);
+    const _addMarker = (e) => { // adds marker to the board, if it is available
+        const chosenCell = e.target;
+        const cellIndex = Number(chosenCell.className.at(-1));
+        if (chosenCell === e.currentTarget) return;
+        if (gameBoard.getBoard()[cellIndex] !== null) return;
+        gameBoard.getBoard()[cellIndex] = 'x';
+        chosenCell.textContent = 'x';
+        console.log(gameBoard.getBoard())
+    }
+    return {getBoard, _addMarker};
 })();
 
 const displayControl = (() => {
-    let xx= 1;
-    const addXX = () => ++xx;
-    const getXX = () => xx;
-    return {addXX, getXX}
-    // const _drawMarker = (chosenCell) => {
-    //     chosenCell.textContent = 'x';
-    // }
-    // return {_drawMarker};
+
+    const _showMarker = (chosenCell) => {
+        chosenCell.textContent = 'x';
+    }
+    return {_showMarker};
 })();
 
 const gameFlowLogic = (() => {
@@ -38,30 +48,17 @@ const gameFlowLogic = (() => {
         console.log(gameBoard._board);
     }
 
-    const _addMarker = (e) => { // adds marker to the board, if it is available
-        const chosenCell = e.target;
-        const cellIndex = Number(chosenCell.className.at(-1));
-        if (chosenCell === e.currentTarget) return;
-        if (gameBoard._board[cellIndex] !== null) return;
-        gameBoard._board[cellIndex] = 'x';
-        chosenCell.textContent = 'x';
-    }
+    // const _addMarker = (e) => { // adds marker to the board, if it is available
+    //     const chosenCell = e.target;
+    //     const cellIndex = Number(chosenCell.className.at(-1));
+    //     if (chosenCell === e.currentTarget) return;
+    //     if (gameBoard._board[cellIndex] !== null) return;
+    //     gameBoard._board[cellIndex] = 'x';
+    //     chosenCell.textContent = 'x';
+    // }
 
     
-
+    // console.log(gameBoard.getBoard())
     _button.addEventListener('click', _choosePlayer);
-    _cells.addEventListener('click', _addMarker);
-})();
-
-console.log(displayControl.addXX())
-console.log(displayControl.getXX())
-
-const counterCreator = () => {
-    let count = 0;
-    return count++
-  }
-  
- const counter = counterCreator;
-
- counter()
- counter()
+    _cells.addEventListener('click', gameBoard._addMarker);
+})(Players, gameBoard, displayControl);
