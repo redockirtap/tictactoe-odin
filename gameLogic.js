aee9bc1ed4c5b2396645ab76aeb416ee78e62551
Partrickoder <redockirtap@gmail.com>
2022-10-29 23:47:35 +0300

    Start making overall styles, menu modal, buttons.

--- gameLogic.js
+++ gameLogic.js
@@ -1,6 +1,5 @@
 const Players = (name, marker) => {
     const player = name || 'John Doe';
-    // const marker = marker || null;
     return {player, marker};
 };
 
@@ -12,18 +11,17 @@ const gameBoard = (() => {
     const getBoard = () => [..._board];
     const addMarker = (position, marker) => {_board[position] = marker, console.log(_board)};
     const isDraw = () => _board.every((cell) => cell !== null);
-    const isWin = (player1, player2, marker, position) => {
+    const isWin = (player1, marker, position) => {
         const winConditionsHorizontal = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
         const winConditionsVertical = [[0, 3, 6], [1, 4, 7], [2, 5, 8]];
         const winConditionsDiagonal = [[2, 4, 6], [0, 4, 8]];
         const AllCombos = winConditionsHorizontal.concat(winConditionsVertical, winConditionsDiagonal); // concat all winning combos
-        // const winCount = []; 
         marker === player1.marker ? currentMovesX.push(position) : currentMovesO.push(position); //where to push marker
         if (AllCombos.some(combo => combo.every(cell => currentMovesX.includes(cell)))
         || AllCombos.some(combo => combo.every(cell => currentMovesO.includes(cell)))) {
             currentMovesX = [];
             currentMovesO = [];
-            console.log('yoo')
+            console.log('yoo');
             return true;
         }
         return false;
@@ -53,18 +51,15 @@ const gameFlowLogic = (() => {
     let player1; // initialize players 
     let player2;
     let counter = 0; // counter for player turns
-    const gameStat = []; // to add wins and loses
+    // const gameStat = []; // to add wins and loses
 
     const choosePlayerHandler = function (e) { // choose player vs player or player vs AI
         const chosenPlayer = e.target || null; // select player buttons
         console.log('choosing game mode...')
         if (chosenPlayer === null || chosenPlayer.className === 'play') return; // OR if button play game is clicked
-        // const player1 = Players('user', 'x');
-        // const player2 = Players(chosenPlayer.textContent, 'o');
         player1 = Players('user', 'x');
         player2 = Players(chosenPlayer.textContent, 'o');
         console.log(player1, player2);
-        // return {player1, player2}
     };
 
     const changeTurn = (position) => {
@@ -76,13 +71,13 @@ const gameFlowLogic = (() => {
     }
 
     const checkForDraw = () => gameBoard.isDraw() ? gameBoard.cleanBoard() && displayControl.cleanBoard() : false;
-    const checkForWin = (player1, player2, marker, position) => gameBoard.isWin(player1, player2, marker, position) ? gameBoard.cleanBoard() && displayControl.cleanBoard() : false;
+    const checkForWin = (player1, marker, position) => gameBoard.isWin(player1, marker, position) ? gameBoard.cleanBoard() && displayControl.cleanBoard() : false;
     const checkForBusy = (position) => gameBoard.isBusy(position) ? true : false;
     const addMarkerHandler = (e, position=Number(e.target.className.at(-1)), marker=changeTurn(position)) => {
         if (checkForBusy(position)) return; // check if cell is busy
         gameBoard.addMarker(position, marker); // store marker to the board array
         displayControl.showMarker(e, marker); // display the marker on the screen
-        if (checkForWin(player1, player2, marker, position)) return; // check if there is a win
+        if (checkForWin(player1, marker, position)) return; // check if there is a win
         if (checkForDraw()) return; // check if the game is over due to draw
     };
     return {choosePlayerHandler, checkForDraw, checkForWin, checkForBusy, addMarkerHandler};
@@ -110,10 +105,7 @@ const eventListenerz =(() => {
 
         // if (_board[0] === marker && _board[1] === marker && _board[2] === marker 
         //     || _board[3] === marker && _board[4] === marker && _board[5] === marker
-        //     || _board[6] === marker && _board[7] === marker && _board[8] === marker) {
-                
-        //         return true
-        //     };
+        //     || _board[6] === marker && _board[7] === marker && _board[8] === marker) return true
         // if (_board[0] === marker && _board[3] === marker && _board[6] === marker 
         //     || _board[1] === marker && _board[4] === marker && _board[7] === marker
         //     || _board[2] === marker && _board[5] === marker && _board[8] === marker) return true;
